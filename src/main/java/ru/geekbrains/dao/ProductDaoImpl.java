856f5e0ef07b313c7e2570af0f4eeab263081f4d
basilomp <k0m4w1@gmail.com>
2022-05-22 21:08:24 +0300

    Assignment 8 completed

--- src/main/java/ru/geekbrains/dao/ProductDaoImpl.java
+++ src/main/java/ru/geekbrains/dao/ProductDaoImpl.java
@@ -14,57 +14,57 @@ import java.util.List;
 @Repository
 @Transactional
 @ComponentScan("ru.geekbrains.session")
-public class ProductDaoImpl implements Dao{
+public abstract class ProductDaoImpl implements Dao{
 
-    @Autowired
-    private SessionFactoryUtils sessionFactoryUtils;
-
-    public ProductDaoImpl(SessionFactoryUtils sessionFactoryUtils) {
-        this.sessionFactoryUtils = sessionFactoryUtils;
-    }
-
-
-    @Override
-    public Product findById(Long id) {
-        try (Session session = sessionFactoryUtils.getSession()){
-            session.beginTransaction();
-            Product product = session.get(Product.class, id);
-            session
-                    .getTransaction()
-                    .commit();
-            return product;
-        }
-    }
-
-    @Override
-    public List<Product> findAll() {
-        try (Session session = sessionFactoryUtils.getSession()) {
-            session.beginTransaction();
-            List<Product> products = session.createQuery("select p from Product p").getResultList();
-            session.getTransaction().commit();
-            return products;
-        }
-    }
-
-    @Override
-    public void deleteById(Long id) {
-        try (Session session = sessionFactoryUtils.getSession()) {
-            session.beginTransaction();
-            Product product = session.get(Product.class, id);
-            session.delete(product);
-            session.getTransaction().commit();
-        }
-    }
-
-    @Override
-    public Product saveOrUpdate(Product product) {
-        try (Session session = sessionFactoryUtils.getSession()) {
-            session.beginTransaction();
-            session.saveOrUpdate(product);
-            session.getTransaction().commit();
-            return product;
-        }
-    }
-
+//    @Autowired
+//    private SessionFactoryUtils sessionFactoryUtils;
+//
+//    public ProductDaoImpl(SessionFactoryUtils sessionFactoryUtils) {
+//        this.sessionFactoryUtils = sessionFactoryUtils;
+//    }
+//
+//
+//    @Override
+//    public Product findById(Long id) {
+//        try (Session session = sessionFactoryUtils.getSession()){
+//            session.beginTransaction();
+//            Product product = session.get(Product.class, id);
+//            session
+//                    .getTransaction()
+//                    .commit();
+//            return product;
+//        }
+//    }
+//
+//    @Override
+//    public List<Product> findAll() {
+//        try (Session session = sessionFactoryUtils.getSession()) {
+//            session.beginTransaction();
+//            List<Product> products = session.createQuery("select p from Product p").getResultList();
+//            session.getTransaction().commit();
+//            return products;
+//        }
+//    }
+//
+//    @Override
+//    public void deleteById(Long id) {
+//        try (Session session = sessionFactoryUtils.getSession()) {
+//            session.beginTransaction();
+//            Product product = session.get(Product.class, id);
+//            session.delete(product);
+//            session.getTransaction().commit();
+//        }
+//    }
+//
+//    @Override
+//    public Product saveOrUpdate(Product product) {
+//        try (Session session = sessionFactoryUtils.getSession()) {
+//            session.beginTransaction();
+//            session.saveOrUpdate(product);
+//            session.getTransaction().commit();
+//            return product;
+//        }
+//    }
+//
 
 }
