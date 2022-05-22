d8bc736d2e89eba718576fff09eb0cfd45352ef1
basilomp <k0m4w1@gmail.com>
2022-05-22 21:06:12 +0300

    Assignment 8 completed

--- src/main/java/ru/geekbrains/dao/CustomerDao.java
+++ src/main/java/ru/geekbrains/dao/CustomerDao.java
@@ -16,50 +16,50 @@ import java.util.List;
 @ComponentScan("ru.geekbrains.session")
 public class CustomerDao {
 
-    @Autowired
-    private SessionFactoryUtils sessionFactoryUtils;
-
-    public CustomerDao(SessionFactoryUtils sessionFactoryUtils) {
-        this.sessionFactoryUtils = sessionFactoryUtils;
-    }
-
-
-    public Customer findById(Long id) {
-        try (Session session = sessionFactoryUtils.getSession()){
-            session.beginTransaction();
-            Customer customer = session.get(Customer.class, id);
-            session
-                    .getTransaction()
-                    .commit();
-            return customer;
-        }
-    }
-
-    public List<Customer> findAll() {
-        try (Session session = sessionFactoryUtils.getSession()) {
-            session.beginTransaction();
-            List<Customer> customers = session.createQuery("select c from Customer c").getResultList();
-            session.getTransaction().commit();
-            return customers;
-        }
-    }
-
-    public void deleteById(Long id) {
-        try (Session session = sessionFactoryUtils.getSession()) {
-            session.beginTransaction();
-            Customer customer = session.get(Customer.class, id);
-            session.delete(customer);
-            session.getTransaction().commit();
-        }
-    }
-
-    public Customer saveOrUpdate(Customer customer) {
-        try (Session session = sessionFactoryUtils.getSession()) {
-            session.beginTransaction();
-            session.saveOrUpdate(customer);
-            session.getTransaction().commit();
-            return customer;
-        }
-    }
+//    @Autowired
+//    private SessionFactoryUtils sessionFactoryUtils;
+//
+//    public CustomerDao(SessionFactoryUtils sessionFactoryUtils) {
+//        this.sessionFactoryUtils = sessionFactoryUtils;
+//    }
+//
+//
+//    public Customer findById(Long id) {
+//        try (Session session = sessionFactoryUtils.getSession()){
+//            session.beginTransaction();
+//            Customer customer = session.get(Customer.class, id);
+//            session
+//                    .getTransaction()
+//                    .commit();
+//            return customer;
+//        }
+//    }
+//
+//    public List<Customer> findAll() {
+//        try (Session session = sessionFactoryUtils.getSession()) {
+//            session.beginTransaction();
+//            List<Customer> customers = session.createQuery("select c from Customer c").getResultList();
+//            session.getTransaction().commit();
+//            return customers;
+//        }
+//    }
+//
+//    public void deleteById(Long id) {
+//        try (Session session = sessionFactoryUtils.getSession()) {
+//            session.beginTransaction();
+//            Customer customer = session.get(Customer.class, id);
+//            session.delete(customer);
+//            session.getTransaction().commit();
+//        }
+//    }
+//
+//    public Customer saveOrUpdate(Customer customer) {
+//        try (Session session = sessionFactoryUtils.getSession()) {
+//            session.beginTransaction();
+//            session.saveOrUpdate(customer);
+//            session.getTransaction().commit();
+//            return customer;
+//        }
+//    }
 }
 
